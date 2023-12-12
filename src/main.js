7adefefdf0477aadea224f0d25f498a1723d7ee5
Amen Mohamed <145727573+WorldisAmen@users.noreply.github.com>
2023-12-12 20:36:06 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'animate.css/animate.min.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 
