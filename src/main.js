ab3ec9c2b00ff5b449d6e20dbdf40e62bfc56a99
Amen Mohamed <145727573+WorldisAmen@users.noreply.github.com>
2023-12-14 09:14:55 +0200

    Update main.js
    added font awesome imports for animated icons

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import 'animate.css/animate.min.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 
+library.add(faSearch);
 createApp(App).use(router).mount('#app')
+app.component('font-awesome-icon', FontAwesomeIcon);
