f246c5397a6ad0ceee8b7ed37454449347a2b98d
RafyHany <rafyhany1@gmail.com>
2023-12-14 16:48:03 +0200

    initialize spring

---
+++ mail/mail/src/main/java/com/example/mailServer/mail/controller/control.java
@@ -0,0 +1,12 @@
+package com.example.mailServer.mail.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@CrossOrigin("*")
+@RequestMapping("/mail")
+public class control {
+
+}
---
+++ mail/mail/src/main/java/com/example/mailServer/mail/services/service.java
@@ -0,0 +1,7 @@
+package com.example.mailServer.mail.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class service {
+}
