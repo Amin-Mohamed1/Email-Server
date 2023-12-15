f8f3a04350e144bf0edb155a7d2f466f8b3fb68c
Ranime Shehata <ranimeshehata@gmail.com>
2023-12-15 03:56:38 +0200

    The "trash" and "folders" components are done

--- src/components/FoldersComponent.vue
+++ src/components/FoldersComponent.vue
@@ -1,21 +1,274 @@
 <template>
-  <div class="newfolderformclass">
-                <!-- <label for="foldername">Create new folder</label>
-                <input type="text"
-                  id="foldername"
-                  class="w3-input"
-                  type="text"
-                  placeholder="Enter folder name"
-                /> -->
-              </div>
+      <h2 class="folders-title">Folders<i class="fa fa-folder" style="margin-left: 10px;"></i></h2>
+    
+      <transition-group name="fade" mode="out-in">
+        <div class="folderdiv">
+          <div class="newfolderform">
+            <label class="create" for="folderName">Create New Folder</label>
+            <input v-model="newFolderName" id="folderName" type="text" placeholder="Enter folder name" />
+          </div>
+          <div>
+            <button class="newfolderbutton" @click="createFolder()">Create new folder <i class="fa fa-plus" style="color: azure; font-size: 18px"></i></button>
+          </div>
+        </div>
+        <div v-for="folderName in folders" :key="folderName" class="foldersdiv">
+          <label class="folderlabel">
+            {{ folderName }}
+            <a class="icon" title="Rename folder" href="javascript:void(0)" @click="showRenameModal(folderName)">
+              <i class="fa fa-pen" style="color: #0056b3; font-size: 20px; margin-left: 20px"></i>
+            </a>
+            <a class="icon" title="Delete folder" href="javascript:void(0)" @click="deleteFolder(folderName)">
+              <i class="fa fa-trash redtrash" style="color: rgb(202, 59, 59); font-size: 20px; margin-left: 10px"></i>
+            </a>
+          </label>
+
+          <table v-if="showFolders" class="table">
+              <thead>
+                <tr>
+                  <th scope="col" class="selectcol" style="width: 50px"></th>
+                  <th scope="col" class="prioritycol">Priority</th>
+                  <th scope="col" class="sendercol">Sender</th>
+                  <th scope="col" class="datecol">Date</th>
+                  <th scope="col" class="subjectcol">Subject</th>
+                  <th scope="col" class="contentcol">Content</th>
+                  <th scope="col" class="downloadcol">Attachment</th>
+                  <th scope="col" class="remove">Remove from folder</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="mail in hash_folder[folderName]" :key="mail.id">
+        <td>
+          <input type="checkbox" :checked="isSelected(mail.id)" @change="onchange(mail.id)" />
+        </td>
+                <!-- <tr v-for="mail in hash_folder[folderName]" :key="mail.id">
+                  <td>
+                    <input type="checkbox" :checked="selectedMails.includes(mail.id)" @change="onchange(mail.id)" />
+ 
+                    <input type="checkbox" v-model="newFolderName" :value="mail.id" @change="onchange(mail.id)" />
+                    <input type="checkbox" name="selected" @change="onchange(mail.id)"/> 
+                  </td> -->
+                  <td>{{ mail.priority }}</td>
+                  <td>{{ mail.sender }}</td>
+                  <td>{{ mail.date }}</td>
+                  <td>{{ mail.subject }}</td>
+                  <td>{{ mail.content }}</td>
+                  <td>
+                    <template v-if="mail.attachements.length !== 0">
+                    <a href="javascript:void(0)" title="Download" @click="showAttachModal(mail)">
+                      Download <i class="fa fa-download"></i>
+                    </a>
+                    </template>
+                    <template v-else>
+                      <label>No Attachments</label>
+                    </template>
+                  </td>
+
+                  <td class="align-middle">
+                    <a href="javascript:void(0)" @click="delMail(mail, '0', folderName)">
+                <i class="fa fa-remove redtrash" style="font-size: 30px"></i>
+                    </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            </div>
+        </transition-group>
 </template>
 
 <script>
-export default {
+import { ref } from 'vue';
 
-}
+export default {
+  data() {
+    return {
+      showFolders: true,
+      folders: ['Discrete', 'Numerical', 'Linear Algebra', 'Calculus'],
+      hash_folder: [{
+        'Discrete': [
+          { id: 1, sender: 'John Doe', subject: 'Meeting Tomorrow', content: 'Lorem ipsum...', priority: 'High', date: '02/01/2023', attachments: [] },
+        ],
+        'Linear Algebra': [
+          { id: 2, sender: 'Jane Smith', subject: 'Presentation Feedback', content: 'Your presentation was great!', priority: 'Medium', date: '03/01/2023', attachments: ['file1.pdf'] },
+        ],
+      },
+      ],
+    newFolderName: '',
+      selectedMails: [],
+    };
+  },
+  methods: {
+    showRenameModal(folderName) {
+      const newName = prompt(`Enter new name for folder "${folderName}":`);
+      if (newName && newName.trim() !== '') {
+        const index = this.folders.indexOf(folderName);
+        if (index !== -1) {
+          // Update folder name in folders array
+          this.folders[index] = newName.trim();
+
+          // Update key in hash_folder object
+          this.hash_folder[newName.trim()] = [...this.hash_folder[folderName]];
+          delete this.hash_folder[folderName];
+        }
+      }
+    },
+    deleteFolder(folderName) {
+      if (confirm(`Are you sure you want to delete folder "${folderName}"?`)) {
+        // Remove folder from the list
+        this.folders.splice(this.folders.indexOf(folderName), 1);
+        // Remove folder from hash_folder
+        delete this.hash_folder[folderName];
+      }
+    },
+     isSelected(mailId) {
+      return this.selectedMails.includes(mailId);
+    },
+    onchange(mailId) {
+      // Toggle the presence of mailId in the selectedMails array
+      const index = this.selectedMails.indexOf(mailId);
+      if (index === -1) {
+        this.selectedMails.push(mailId);
+      } else {
+        this.selectedMails.splice(index, 1);
+      }
+
+      // Placeholder logic for handling checkbox change event
+      console.log(`Selected mails:`, this.selectedMails);
+    },
+    showAttachModal(mail) {
+      // Placeholder logic for showing attachment modal
+      alert(`Attachment modal for mail with subject "${mail.subject}"`);
+    },
+    delMail(mail, param1, folderName) {
+      if (confirm(`Are you sure you want to remove the mail with subject "${mail.subject}" from folder "${folderName}"?`)) {
+        // Find index of mail in the folder
+        const index = this.hash_folder[folderName].indexOf(mail);
+        if (index !== -1) {
+          // Remove mail from the folder
+          this.hash_folder[folderName].splice(index, 1);
+        }
+      }
+    },
+    
+    createFolder() {
+      const trimmedFolderName = this.newFolderName.trim();
+      if (trimmedFolderName !== '') {
+        this.folders.push(trimmedFolderName);
+        this.hash_folder[trimmedFolderName] = [];
+        this.newFolderName = '';
+      }
+    }
+  }
+};
 </script>
 
-<style>
 
+<style>
+.folders-title {
+    font-size: 30px;
+    margin-bottom: 20px;
+    color: #333;
+  }
+  
+  .folderdiv {
+    display: flex;
+    flex-direction: row;
+    background-color: #4196f1;
+    padding: 15px;
+    margin-bottom: 10px;
+    border-radius: 5px;
+    border: #333 4px outset;
+  }
+  
+  .folderdiv:hover{
+   transform: scale(1.02);
+}
+  .create{
+    color: azure;
+    font-size: 30px;
+    margin-right: 10px;
+    font-weight: normal;
+  }
+
+  .newfolderform {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .newfolderform:hover{
+   transform: scale(1.02);
+}
+
+  input {
+    width: 300px;
+    height: 30px;
+    border-radius: 5px;
+    border: none;
+    padding-left: 10px;
+    font-size: 20px;
+    margin-left: 15px;
+  }
+
+  .newfolderbutton {
+    background-color: #0056b3;
+    color: azure;
+    font-size: 20px;
+    border: none;
+    border-radius: 5px;
+    padding: 10px;
+    margin-left: 25px;
+    cursor: pointer;
+  }
+
+  .newfolderbutton:hover {
+    background-color: #003d80;
+  }
+
+  .table:hover{
+   transform: scale(1.02);
+}
+
+.foldersdiv {
+  display: flex;
+  flex-direction: row;
+  background-color: #4196f1;
+  padding: 15px;
+  margin-bottom: 10px;
+  border-radius: 5px;
+}
+
+.foldersdiv:hover{
+   transform: scale(1.02);
+}
+
+.folderlabel {
+  font-size: 20px;
+  color: azure;
+  margin-right: 10px;
+  font-weight: bold;
+}
+
+i:hover{
+  color: #0056b3;
+  transform: scale(1.02);
+}
+
+.icon{
+  display: inline-flex;
+    flex-direction: row;
+}
+
+table, th, td {
+  border: 1px solid black;
+  text-align: center;
+}
+
+table {
+  border-collapse: collapse;
+  width: 100%;
+  font-weight: bold;
+  color: #0a0c0e;
+  font-size: 16px;
+  margin: 0px;
+}
 </style>
--- src/components/TrashComponent.vue
+++ src/components/TrashComponent.vue
@@ -1,16 +1,17 @@
 <template>
-     <h2 class="trash-title">Trash<i class="fa fa-trash redtrash" style="margin-left: 10px; font-size: 40px; color: black"></i> </h2>
-        <transition-group name="fade" mode="out-in">
-           <table v-if="showTrash" class="table table-dark table-bordered table-striped-columns table-hover">
+     <h2 class="trash-title">Trash<i class="fa fa-trash redtrash" style="margin-left: 10px;"></i></h2>
+    
+      <transition-group name="fade" mode="out-in">
+        <table v-if="showTrash" class="table ">
         <thead>
           <tr>
-            <th scope="col" class="selectcol" style="width: 50px"></th>
+            <th scope="col" class="selectcol"></th>
             <th scope="col" class="prioritycol">Priority</th>
             <th scope="col" class="sendercol">Sender</th>
             <th scope="col" class="datecol">Date</th>
             <th scope="col" class="subjectcol">Subject</th>
             <th scope="col" class="contentcol">Content</th>
-            <th scope="col">Move to trash</th>
+            <th scope="col" class="trashcol">Delete Forever</th>
           </tr>
         </thead>
         <tbody>
@@ -19,13 +20,13 @@
               <input type="checkbox" v-model="selectedMails" :value="mail.id" @change="onchange" />
             </td>
             <td>{{ mail.priority }}</td>
-            <td>{{ mail.receiver }}</td>
+            <td>{{ mail.sender }}</td>
             <td>{{ mail.date }}</td>
             <td>{{ mail.subject }}</td>
             <td>{{ mail.content }}</td>
             <td class="align-middle">
               <a href="javascript:void(0)" @click="totalyRemove(mail)">
-                <i class="fa fa-trash redtrash"></i>
+                <i id = "icon" class="fa fa-trash redtrash"></i>
               </a>
             </td>
           </tr>
@@ -36,23 +37,34 @@
 
 <script>
 import { ref } from 'vue';
+
+
 export default {
-    data() {
-        return {
-            showTrash: true,
-            mails: [], // Assuming mails is an array of objects
-            selectedMails: []
-        };
+  data() {
+    return {
+      showTrash: true,
+      mails: [
+        { id: 1, sender: 'Albert.Einstein@gmail.com', subject: 'Meeting Tomorrow', content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', priority: 'Medium', date: '02/01/2023' },
+        { id: 2, sender: 'Robert.Oppenheimer@gmail.com', subject: 'Presentation Feedback', content: 'Your presentation was excellent!', date: '05/11/2023', priority: 'Medium' },
+        { id: 3, sender: 'Neils.Bohr@gmail.com', subject: 'Meeting Tomorrow', content: 'Hi, let\'s discuss the project tomorrow.', date: '01/01/2023', priority: 'High' },
+        { id: 4, sender: 'Lionel.Messi@gmail.com', subject: 'Presentation Feedback', content: 'Your presentation was excellent!', date: '02/01/2023', priority: 'Medium' },
+      ], // Assuming mails is an array of objects
+      selectedMails: []
+    };
+  },
+  methods: {
+    onchange() {
+      // Handle checkbox change event
+      // You can access selectedMails array here
+      console.log(`Checkbox with id ${mailId} changed.`);
     },
-    methods: {
-        onchange() {
-            // Handle checkbox change event
-            // You can access selectedMails array here
-        },
-        totalyRemove(mail) {
-            // Handle removing the mail
-        }
+    totalyRemove(mail) {
+      const index = this.mails.findIndex(m => m.id === mail.id);
+      if (index !== -1) {
+        this.mails.splice(index, 1); // Remove the mail from the array
+      }
     }
+  }
 };
 </script>
 
@@ -65,19 +77,21 @@ export default {
   }
 
   .prioritycol {
-  width: 80px;
+  width: 100px;
   font-weight: bold;
-    color: #007BFF;
-    font-size: 20px;
-    margin: 0px;
+  color: #007BFF;
+  font-size: 20px;
+  margin: 0px;
 }
 .contentcol {
-  width: 300px;
+  width: 500px;
   font-weight: bold;
     color: #007BFF;
     font-size: 20px;
     margin: 0px;
+    
 }
+
 .sendercol {
   width: 250px;
   font-weight: bold;
@@ -85,36 +99,32 @@ export default {
     font-size: 20px;
     margin: 0px;
 }
-.recievercol {
-  width: 250px;
-  font-weight: bold;
-    color: #007BFF;
-    font-size: 20px;
-    margin: 0px;
-}
+
 .subjectcol {
   width: 250px;
-  font-weight: bold;
-    color: #007BFF;
+  font-weight: bold;  
+  color: #007BFF;
     font-size: 20px;
     margin: 0px;
 }
+
 .datecol {
-  width: 100px;
+  width: 180px;
   font-weight: bold;
     color: #007BFF;
     font-size: 20px;
     margin: 0px;
 }
 .selectcol {
-  width: 140px;
+  width: 20px;
   font-weight: bold;
-    color: #007BFF;
-    font-size: 20px;
-    margin: 0px;
+  color: #007BFF;
+  font-size: 10px;
+  margin: 0px;
 }
+
 .trashcol {
-  width: 50px;
+  width: 20px;
   font-weight: bold;
     color: #007BFF;
     font-size: 20px;
@@ -125,15 +135,30 @@ table {
   border-collapse: collapse;
   width: 100%;
   font-weight: bold;
-    color: #007BFF;
-    font-size: 20px;
-    margin: 0px;
+  color: #0a0c0e;
+  font-size: 20px;
+  margin: 0px;
 }
 table, th, td {
-  border: 1px solid;
+  border: 2px solid black;
+  text-align: center;
 }
 .table:hover{
    transform: scale(1.02);
+}
 
+#icon{
+  font-size: 40px;
+  color:rgb(202, 59, 59);
+  margin: 60px;
+}
+
+#icon:hover{
+  color: #007BFF;
+  transform:scale(1.02) ;
+}
+
+input{
+  margin: 5%;
 }
 </style>
