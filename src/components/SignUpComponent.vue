9b0c931dafa0363b142f17f91020cfeab68330e6
Amen Mohamed <145727573+WorldisAmen@users.noreply.github.com>
2023-12-17 20:34:07 +0200

    Added fetch and validation of user's Info (Sign up)

--- src/components/SignUpComponent.vue
+++ src/components/SignUpComponent.vue
@@ -1,111 +1,131 @@
 <template>
   <header class="header-bar">
-        <div class="logo-container" @click="goToHomePage">
-          <div class="logo">
-            <img src="@/assets/logo.png" alt="Gmail Logo" class="logo-image" />
-            <span class="logo-text">Gmail</span>
-          </div>
-        </div>
+    <div class="logo-container" @click="goToHomePage">
+      <div class="logo">
+        <img src="@/assets/logo.png" alt="Gmail Logo" class="logo-image" />
+        <span class="logo-text">Gmail</span>
+      </div>
+    </div>
   </header>
 
   <div class="parent">
-      <div class="child">
-        <main>
-          <div class="wrapper">
-            <h2>Registration</h2>
-            <form action="#">
-              <div class="input-box">
-                <input type="text" placeholder="Enter your name" required>
-              </div>
-              <div class="input-box">
-                <input type="email" placeholder="Enter your email" required>
-              </div>
-              <div class="input-box">
-                <input v-model="birthdate" type="date" id="birthdate" required>
-              </div>
-              <div class="input-box">
-                <input type="password" placeholder="Create password" required>
-              </div>
-              <div class="input-box">
-                <input type="password" placeholder="Confirm password" required>
-              </div>
-              <div class="policy">
-                <input type="checkbox">
-                <h3>I accept all terms & conditions</h3>
-              </div>
-              <div class="input-box button">
-                <input type="Submit" value="Register Now">
-              </div>
-              <div class="text">
-                <h3>Already have an account? <router-link to="/login">Login</router-link></h3>
-              </div>
-            </form>
-          </div>
-        </main>
-      </div>
+    <div class="child">
+      <main>
+        <div class="wrapper">
+          <h2>Registration</h2>
+          <form @submit.prevent="validateSignup">
+            <div class="input-box">
+              <input v-model="username" type="text" placeholder="Enter your name" required>
+            </div>
+            <div class="input-box">
+              <input v-model="emailaccount" type="email" placeholder="Enter your email" required>
+            </div>
+            <div class="input-box">
+              <input v-model="birth" type="date" id="birthdate" required>
+            </div>
+            <div class="input-box">
+              <input v-model="password" type="password" placeholder="Create password" required>
+            </div>
+            <div class="input-box">
+              <input v-model="confirmPassword" type="password" placeholder="Confirm password" required>
+            </div>
+            <div class="policy">
+              <input v-model="acceptTerms" type="checkbox">
+              <h3>I accept all terms & conditions</h3>
+            </div>
+            <div class="input-box button">
+              <input type="submit" value="Register Now">
+            </div>
+            <div class="text">
+              <h3>Already have an account? <router-link to="/login">Login</router-link></h3>
+            </div>
+          </form>
+        </div>
+      </main>
+    </div>
   </div>
 </template>
+
 <script setup>
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
 
-let name = ref('');
-let email = ref('');
+let username = ref('');
+let emailaccount = ref('');
 let password = ref('');
 let confirmPassword = ref('');
-let birthdate = ref('');
+let birth = ref('');
 let acceptTerms = ref(false);
 
-const validateSignup = () => {
-
-  if (password !== confirmPassword) {
+const validateSignup = async () => {
+  if (password.value !== confirmPassword.value) {
+    console.log("password " + password.value);
+    console.log("confirmPassword " + confirmPassword.value);
     alert('Passwords do not match.');
     return;
   }
-
-  const validBirthdate = isValidBirthdate(birthdate);
+  const validBirthdate = isValidBirthdate(birth.value);
+  console.log("isValidBirthdate " + validBirthdate);
   if (!validBirthdate) {
-    alert('Invalid birth date. Please enter a valid birth date.');
+    alert('Invalid birth date. Please enter a valid birth date above 18 years.');
     return;
   }
-
-  fetch('http://localhost/signup', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      name: name.value,
-      email: email.value,
-      password: password.value,
-      birthdate: birthdate.value,
-      acceptTerms: acceptTerms.value,
-    }),
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then(data => {
-      const token = data.token;
-      console.log(token);
-      goToProfilePage();
-    })
-    .catch(error => {
-      console.error('Signup failed:', error);
+  if (!acceptTerms) {
+    alert('Please accept the terms and conditions.');
+    return;
+  }
+  try {
+    const response = await fetch('http://localhost:8081/mail/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: username.value,
+        emailaccount: emailaccount.value,
+        password: password.value,
+        birth: birth.value,
+      }),
     });
+    if (!response.ok) {
+      const errorMessage = await response.text() || 'Unknown error';
+      alert(`HTTP error! Status: ${response.status}, Message: ${errorMessage}`);
+      return;
+    }
+    const responseData = await response.text();
+    console.log(responseData);
+    const message = responseData;
+    if (message === 'Signed up successfully') {
+      alert('Signup successful!');
+      goToProfilePage();
+    } else {
+      alert(`Signup failed: ${message}`);
+    }
+  } catch (error) {
+    console.error('Signup failed:', error);
+    alert('An unexpected error occurred. Please try again.');
+  }
 };
 
-const isValidBirthdate = (birthdate) => {
-  
-  return true;
+const isValidBirthdate = (selectedDate) => {
+    const today = new Date();
+    const birthdate = new Date(selectedDate);
+
+    if (isNaN(birthdate.getTime()) || birthdate > today) {
+        return false; 
+    }
+
+    const age = today.getFullYear() - birthdate.getFullYear();
+    return age >= 18;
 };
+
+const goToProfilePage = () => {
+  router.push('/profile');
+};
+
 </script>
-
 <style scoped>
 @import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap');
   .parent {
@@ -167,11 +187,11 @@ body{
   align-items: center;
   justify-content: center;
   background: #4070f4;
-  animation: fadeIn 1s ease-in-out;
+  animation: fadeIn 1s ease-in-out; /* Fade-in animation */
 }
 .wrapper {
   position: relative;
-  max-width: 400px;
+  max-width: 400px; /* Adjust the width as needed */
   width: 100%;
   background: #fff;
   padding: 34px;
@@ -181,7 +201,7 @@ body{
   opacity: 0;
   margin: auto; /* Center the form */
   animation: slideIn 1s forwards ease-in-out;
-  transform: translateY(20px);
+  transform: translateY(20px); /* Adjust this value to move it upward */
 }
 .wrapper h2{
   position: relative;
@@ -228,7 +248,7 @@ form .input-box input{
 form .policy{
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: center; /* Center the policy checkbox and text */
   margin-top: 20px;
 }
 form h3{
@@ -262,6 +282,7 @@ form .text h3 a:hover{
   text-decoration: underline;
 }
 
+/* Animations */
 @keyframes fadeIn {
   from {
     opacity: 0;
