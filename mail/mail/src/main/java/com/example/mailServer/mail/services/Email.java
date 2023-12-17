7e2a63a1a550b339d33805987a0fa6606c0cd571
AndrewSafwat1 <98947690+AndrewSafwat1@users.noreply.github.com>
2023-12-17 14:42:17 +0200

    implemented Builder Design Pattern via EmailBuilder class

--- mail/mail/src/main/java/com/example/mailServer/mail/services/Email.java
+++ mail/mail/src/main/java/com/example/mailServer/mail/services/Email.java
@@ -3,8 +3,11 @@ package com.example.mailServer.mail.services;
 import java.util.List;
 
 public class Email {
+    //required params
     private String sender;
     private List<String> reciever;
+
+    //optional params
     private String id;
     private String subject;
     private String body;
@@ -14,67 +17,50 @@ public class Email {
 
     public Email(){}
 
+    public Email(EmailBuilder builder){
+        this.sender = builder.getSender();
+        this.reciever = builder.getReciever();
+        this.id = builder.getId();
+        this.subject = builder.getSubject();
+        this.body = builder.getBody();
+        this.attatchments = builder.getAttatchments();
+        this.priority = builder.getPriority();
+        this.dateTime = builder.getDateTime();
+    }
+
     public String getSender() {
         return sender;
     }
 
-    public void setSender(String sender) {
-        this.sender = sender;
-    }
 
     public List<String> getReciever() {
         return reciever;
-    }
-
-    public void setReciever(List<String> reciever) {
-        this.reciever = reciever;
     }
 
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getSubject() {
         return subject;
     }
 
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
 
     public String getBody() {
         return body;
     }
 
-    public void setBody(String body) {
-        this.body = body;
-    }
 
     public List<String> getAttatchments() {
         return attatchments;
-    }
-
-    public void setAttatchments(List<String> attatchments) {
-        this.attatchments = attatchments;
     }
 
     public String getPriority() {
         return priority;
     }
 
-    public void setPriority(String priority) {
-        this.priority = priority;
-    }
-
     public String getDateTime() {
         return dateTime;
     }
 
-    public void setDateTime(String dateTime) {
-        this.dateTime = dateTime;
-    }
 }
---
+++ mail/mail/src/main/java/com/example/mailServer/mail/services/EmailBuilder.java
@@ -0,0 +1,89 @@
+package com.example.mailServer.mail.services;
+
+import java.util.List;
+
+public class EmailBuilder extends Email{
+    //required params
+    private String sender;
+    private List<String> reciever;
+
+    //optional params
+    protected String id;
+    private String subject;
+    private String body;
+    private List<String> attatchments;
+    private String priority;
+    private String dateTime;
+
+    public EmailBuilder(){}
+    public EmailBuilder(String sender, List<String> reciever){
+        this.sender = sender;
+        this.reciever = reciever;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public List<String> getReciever() {
+        return reciever;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public List<String> getAttatchments() {
+        return attatchments;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public EmailBuilder setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public EmailBuilder setSubject(String subject) {
+        this.subject = subject;
+        return this;
+    }
+
+    public EmailBuilder setBody(String body) {
+        this.body = body;
+        return  this;
+    }
+
+    public EmailBuilder setAttatchments(List<String> attatchments) {
+        this.attatchments = attatchments;
+        return this;
+    }
+
+    public EmailBuilder setPriority(String priority) {
+        this.priority = priority;
+        return this;
+    }
+
+    public EmailBuilder setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+        return this;
+    }
+
+    public Email build(){
+        return new Email(this);
+    }
+}
