ddb3f4151ffbde3c714e0c33d7da324275e2b701
Amen Mohamed <145727573+WorldisAmen@users.noreply.github.com>
2023-12-19 19:37:16 +0200

    added cookies in profile component

--- src/components/ProfileComponent.vue
+++ src/components/ProfileComponent.vue
@@ -7,6 +7,16 @@
         <span>Gmail</span>
       </div>
 
+      <div class="user-info">
+        <div class="user-avatar">
+          <i class="fas fa-user-circle"></i>
+        </div>
+        <div class="user-details">
+          <p class="username" @mouseover="highlightText" @mouseleave="resetText">{{ username }}</p>
+        </div>
+      </div>
+
+
       <nav class="nav-links">
           <router-link to="/profile/compose" class="nav-link" style="color: #3498db;" active-class="router-link-active">
             <i class="fas fa-pen animated-icon"></i> Compose
@@ -47,6 +57,18 @@
       <div class = "utility" >
         <FilterComponent />
       </div>
+      <!--
+      <div>
+        <p>Folders:</p>
+        <ul>
+          <li v-for="(folder, index) in folders" :key="index">
+            {{ folder.name }} - Emails: {{ folder.emails?.length }}
+          </li>
+        </ul>
+        <p>Contacts: {{ contacts }}</p>
+        <p>ID Message: {{ idMessage }}</p>
+      </div>
+    -->
       <router-view></router-view>
     </main>
   </div>
@@ -54,14 +76,37 @@
 
 <script setup>
 import { useRouter } from 'vue-router';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import FilterComponent from './FilterComponent.vue';
 const router = useRouter();
 const isSidebarExpanded = ref(false);
+const username = ref('');
+const email = ref('');
+const folders = ref([]); 
+const contacts = ref([]);
+const idMessage = ref(0);
+onMounted(() => {
+  const cookies = document.cookie.split('; ');
+  for (const cookie of cookies) {
+    const [name, value] = cookie.split('=');
+    if (name === 'username') {
+      username.value = value;
+    } else if (name === 'email') {
+      email.value = value;
+    }
+    else if(name === 'idMessage'){
+      idMessage.value = parseInt(value);
+      console.log(idMessage.value);
+    }
+     else if(name === 'folders'){
+      folders.value = JSON.parse(value);
+      console.log(value);
+    }else if(name === 'contacts'){
+      contacts.value = JSON.parse(value);
+    }
+  }
+});
 
-const goToHomePage = () => {
-  router.push('/home');
-};
 
 const expandSidebar = () => {
   isSidebarExpanded.value = true;
@@ -70,6 +115,8 @@ const expandSidebar = () => {
 const collapseSidebar = () => {
   isSidebarExpanded.value = false;
 };
+
+
 </script>
 
 <style scoped>
@@ -151,6 +198,54 @@ const collapseSidebar = () => {
   100% {
     transform: scale(1);
   }
+}
+.user-info {
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.user-avatar {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px; /* Adjust the size of the Font Awesome icon */
+  color: #1e0c93; /* Text color */
+  background-color: transparent !important;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Optional: Add a subtle box shadow */
+}
+.user-details {
+  flex: 1;
+  margin-left: 10px; /* Add some spacing between avatar and details */
+}
+
+.username,
+.email {
+  font-family: 'Arial', sans-serif; /* Set font to Arial */
+  cursor: pointer; /* Add cursor style for hoverable text */
+  transition: color 0.3s; /* Smooth transition for text color change */
+}
+
+.username:hover {
+  color: #3498db; /* Change text color on hover */
+}
+
+.username {
+  font-size: 18px;
+  font-weight: bold;
+  color: #333; /* Dark text color */
+}
+.nav-links .nav-link .animated-icon {
+  margin-right: 12px;
+  font-size: 1.2em; /* Adjust the size of the Font Awesome icon */
+  transition: transform 0.2s ease-out;
+}
+
+.user-avatar span {
+  font-size: 1.5em; /* Adjust the size of the Font Awesome icon */
 }
 
 .main-content {
