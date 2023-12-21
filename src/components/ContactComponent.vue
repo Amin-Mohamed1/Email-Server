a60a8bed1b793064c838ecb052b90d8183a0693f
Amen Mohamed <145727573+WorldisAmen@users.noreply.github.com>
2023-12-21 15:55:01 +0200

    add , edit , delete is handled in contact component
    install axios

--- src/components/ContactComponent.vue
+++ src/components/ContactComponent.vue
@@ -6,15 +6,13 @@
         <input type="text" id="name" v-model="formData.name" required placeholder="Enter name">
       </div>
       <div class="input-box">
-        <input type="email" id="email" v-model="formData.email" required placeholder="Enter email">
+        <input type="email" id="emailaccount" v-model="formData.emailaccount" required placeholder="Enter emailaccount">
       </div>
       <div class="input-box button">
         <button type="submit" class="submit-btn">Add Contact</button>
       </div>
     </form>
 
-
-    
     <h2>Contact</h2>
     <table class="user-list">
       <thead>
@@ -27,7 +25,7 @@
       <tbody>
         <tr v-for="(user, index) in userList" :key="index" class="hoverable-row">
           <td>{{ user.name }}</td>
-          <td>{{ user.email }}</td>
+          <td>{{ user.emailaccount }}</td>
           <td>
             <button @click="editContact(user.id)" class="edit-btn">Edit</button>
             <button @click="deleteContact(user.id)" class="delete-btn">Delete</button>
@@ -36,136 +34,125 @@
       </tbody>
     </table>
   </div>
-
-  <p>Contact Information: {{ profileContactInfo  }}</p>
-  <p>Contact Information: {{ userList.length }}</p>
-  
 </template>
 
-<script setup>
+<script>
+import { defineComponent } from 'vue';
+import axios from 'axios';
 
-import { ref } from 'vue';
+export default defineComponent({
+  props: {
+    profileContactInfo: String,
+    userList: Array,
+  },
+  data() {
+    return {
+      formData: {
+        name: '',
+        emailaccount: '',
+      },
+      nextId: 1,
+    };
+  },
+  methods: {
+    async submitForm() {
+      const addedUser = {
+        id: this.nextId,
+        name: this.formData.name,
+        emailaccount: this.formData.emailaccount,
+      };
+      this.userList.push(addedUser);
+      this.nextId += 1;
+      this.formData = {
+        name: '',
+        emailaccount: '',
+      };
 
-const formData = ref({
-  name: '',
-  email: '',
+      await this.sendAddedDataToBackend(addedUser);
+    },
+    async sendAddedDataToBackend(addedUser) {
+      const account = this.profileContactInfo;
+
+      try {
+        const response = await fetch(`http://localhost:8081/mail/addContact/${account}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          id: addedUser.id,
+          emailaccount: addedUser.emailaccount,
+          name: addedUser.name
+        }),
+      });
+      console.log(response.data);
+     }catch(error){
+      console.log(error + " why!");
+     }
+    },
+    deleteContact(id) {
+      const index = this.userList.findIndex(contact => contact.id === id);
+      if (index !== -1) {
+        const deletedUser = this.userList[index];
+        this.userList.splice(index, 1);
+        this.sendDeletedDataToBackend(deletedUser);
+      }
+    },
+    editContact(id) {
+      const index = this.userList.findIndex(contact => contact.id === id);
+      if (index !== -1) {
+        const newName = prompt('Enter the new name:', this.userList[index].name);
+        const newEmail = prompt('Enter the new email:', this.userList[index].emailaccount);
+        if (newName !== null && newEmail !== null) {
+          this.userList[index].name = newName;
+          this.userList[index].emailaccount = newEmail;
+          this.sendEditedDataToBackend(this.userList[index]);
+        }
+      }
+    },
+    async sendEditedDataToBackend(editedUser) {
+      const account = this.profileContactInfo;
+      console.log(editedUser.name);
+      try {
+        const response = await fetch(`http://localhost:8081/mail/editContact/${account}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          id: editedUser.id,
+          emailaccount: editedUser.emailaccount,
+          name: editedUser.name
+        }),
+      });
+      console.log(response.data);
+     }catch(error){
+      console.log(error + " why!");
+     }
+    },
+
+    async sendDeletedDataToBackend(deletedUser) {
+      const account = this.profileContactInfo;
+        try {
+          const response = await fetch(`http://localhost:8081/mail/deleteContact/${account}/${deletedUser.id}`, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+
+          }),
+        });
+        console.log(response.data);
+      }catch(error){
+        console.log(error + " why!");
+      }
+    },
+
+  },
 });
-const nextId = ref(1); 
-
-const submitForm = () => {
-  const addedUser = {
-    id: nextId.value,
-    name: formData.value.name,
-    email: formData.value.email,
-  };
-
-  userList.value.push(addedUser);
-  nextId.value += 1;
-  formData.value = {
-    name: '',
-    email: '',
-  };
-  sendAddedDataToBackend(addedUser);
-};
-
-const sendAddedDataToBackend = async (addedUser) => {
-  console.log('Sending added data to the backend:', addedUser);
-  const gmail = profileContactInfo.value;
-  console.log(profileContactInfo.value);
-  try {
-    await fetch('http://localhost/add', {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        gmail: gmail,
-        id: addedUser.id,
-        name: addedUser.name,
-        email: addedUser.email,
-      }),
-    });
-
-    console.log('Data successfully sent to backend');
-  } catch (error) {
-    console.error('Error sending data to backend:', error.message);
-  }
-};
-
-const deleteContact = (id) => {
-  const index = userList.value.findIndex(contact => contact.id === id);
-  console.log(id);
-  if (index !== -1) {
-    userList.value.splice(index, 1);
-    sendDeletedDataToBackend(userList.value[index]);
-  }
-};
-
-const editContact = (id) => {
-  const index = userList.value.findIndex(contact => contact.id === id);
-  console.log(id);
-  if (index !== -1) {
-    const newName = prompt('Enter the new name:', userList.value[index].name);
-    const newEmail = prompt('Enter the new email:', userList.value[index].email);
-
-    if (newName !== null && newEmail !== null) {
-      userList.value[index].name = newName;
-      userList.value[index].email = newEmail;
-      sendEditedDataToBackend(userList.value[index]);
-    }
-  }
-};
-
-import { defineProps } from 'vue';
-
-
-const props = defineProps(['profileContactInfo','userList']);
-const sendEditedDataToBackend = async (editedUser) => {
-  console.log('Sending edited data to the backend:', editedUser);
-  const gmail = props.profileContactInfo;
-  console.log(gmail);
-  try {
-    await fetch('http://localhost/update', {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        gmail: gmail,
-        id: editedUser.id,
-        name: editedUser.name,
-        email: editedUser.email,
-      }),
-    });
-
-    console.log('Data successfully sent to backend');
-  } catch (error) {
-    console.error('Error sending data to backend:', error.message);
-  }
-};
-
-const sendDeletedDataToBackend = async (deletedUser) => {
-  console.log('Sending deleted data to the backend:', deletedUser);
-  const gmail = profileContactInfo.value;
-  console.log(gmail);
-  try {
-    await fetch('http://localhost/delete', {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        gmail: gmail,
-        id: deletedUser.id,
-      }),
-    });
-
-    console.log('Data successfully sent to backend');
-  } catch (error) {
-    console.error('Error sending data to backend:', error.message);
-  }
-};
 </script>
+
 
 <style scoped>
 .contact-container {
@@ -173,17 +160,14 @@ const sendDeletedDataToBackend = async (deletedUser) => {
   margin: auto;
   padding: 20px;
 }
-
 .contact-form {
   max-width: 400px;
   margin: auto;
   margin-bottom: 20px;
 }
-
 .input-box {
   margin: 12px 0;
 }
-
 input {
   height: 100%;
   width: 100%;
@@ -196,12 +180,10 @@ input {
   border-radius: 6px;
   transition: all 0.3s ease;
 }
-
 input:focus,
 input:valid {
   border-color: #4070f4;
 }
-
 .submit-btn,
 .edit-btn,
 .delete-btn {
@@ -214,52 +196,40 @@ input:valid {
   transition: background-color 0.3s;
   margin-right: 5px ;
 }
-
 .submit-btn {
   background: #4070f4;
   margin-left: 170px ;
 }
-
 .submit-btn:hover {
   background: #0e4bf1;
 }
-
 .edit-btn {
   background: #4caf50;
 }
-
 .edit-btn:hover {
   background: #45a049;
 }
-
 .delete-btn {
   background: #ff5555;
 }
-
 .delete-btn:hover {
   background: #dd4444;
 }
-
 .user-list {
   width: 100%;
   border-collapse: collapse;
   margin-top: 20px;
 }
-
 th,
 td {
   border: 1px solid #ddd;
   padding: 12px;
   text-align: left;
 }
-
 th {
   background-color: #f2f2f2;
 }
-
 .hoverable-row:hover {
   background-color: #f0f0f0;
 }
 </style>
-
-  
