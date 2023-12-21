f23f594b8f09aa4422af7a0e5b1aad17437667a4
AndrewSafwat1 <98947690+AndrewSafwat1@users.noreply.github.com>
2023-12-21 16:53:18 +0200

    Criteria factory is done

---
+++ mail/mail/src/main/java/com/example/mailServer/mail/services/FilterEmail/CriteriaFactory.java
@@ -0,0 +1,23 @@
+package com.example.mailServer.mail.services.FilterEmail;
+
+import java.util.ArrayList;
+
+public class CriteriaFactory {
+    static final int FIRSTELEMENT = 0;
+    public ICriteria getCriteria(String criteriaName, ArrayList<String> criteriaValue){
+        if (criteriaName == null)
+            return null;
+        else if (criteriaName.equalsIgnoreCase("DATETIME"))
+            return new CriteriaDateTime(criteriaValue.get(FIRSTELEMENT));
+        else if (criteriaName.equalsIgnoreCase("PRIORITY"))
+            return new CriteriaPriority(criteriaValue.get(FIRSTELEMENT));
+        else if (criteriaName.equalsIgnoreCase("RECIEVER"))
+            return new CriteriaReciever(criteriaValue);
+        else if (criteriaName.equalsIgnoreCase("SENDER"))
+            return new CriteriaSender(criteriaValue.get(FIRSTELEMENT));
+        else if (criteriaName.equalsIgnoreCase("SUBJECT"))
+            return new CriteriaSubject(criteriaValue.get(FIRSTELEMENT));
+        else
+            return null;
+    }
+}
