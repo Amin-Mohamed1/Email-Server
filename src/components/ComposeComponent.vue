87ac30a521fe6596a2b2f02e9871ed40aafff142
Ranime Shehata <ranimeshehata@gmail.com>
2023-12-22 16:06:30 +0200

    send email & save drafts methods are done. Moreover, sendEmail fetch is done successfully.

--- src/components/ComposeComponent.vue
+++ src/components/ComposeComponent.vue
@@ -1,162 +1,356 @@
 <template>
-    <h2 class="compose-title">New Message <i class="fa fa-envelope" style="margin-left: 10px; font-size: 40px; color: black"></i> </h2>
-  <transition-group name="fade" mode="out-in">
-    <div class="sender" >
-      <label for="to">To :</label>
-      <input v-model="to" id="to" type="email" name="to" placeholder="Enter recipient's email address" required/>
+  <div class="compose-container">
+    <div class="header">
+      <h2>
+        <i class="fas fa-envelope"></i> Compose Email
+      </h2>
+      <div class="input-group to-group">
+        <label for="to">
+          <i class="fas fa-users"></i> To:
+        </label>
+        <div class="to-info">
+          <ul>
+            <li v-for="(user, index) in to" :key="index">
+              <i class="fas fa-user"></i> {{ user.email }}
+            </li>
+          </ul>
+          <div class="add-recipient" @click="openAddUserModal">
+            <i class="fas fa-plus"></i>
+          </div>
+        </div>
+      </div>
+      <div class="input-group from-group">
+        <label for="from">
+          <i class="fas fa-user"></i> From:
+        </label>
+        <input type="text" id="from" v-model="from" :placeholder="profileContactInfo" :readonly="true" />
+      </div>
     </div>
 
-    <div class="subject">
-      <label for="subject">Subject :</label>
-      <input v-model="subject" id="subject" type="text" name="subject" maxlength="30" placeholder="Enter subject here (max 30 characters)" required/>
+    <textarea v-model="emailBody" placeholder="Compose your email..." class="compose-textarea"></textarea>
+
+    <div v-if="showAddUserModal" class="modal">
+      <div class="modal-content">
+        <span class="close" @click="closeAddUserModal">&times;</span>
+        <label for="newUserEmail">Enter User Gmail:</label>
+        <input type="text" id="newUserEmail" v-model="newUserEmail" placeholder="Enter user's email" />
+        <button @click="addUser" class="add-user-button">Add User</button>
+      </div>
     </div>
 
-    <textarea v-model="message" class="messagebox" id="message" name="message" placeholder="Enter your message here .."  required></textarea>
+    <div class="attachment-container">
+      <label for="attachments">
+        <i class="fas fa-paperclip"></i> Choose Attachments:
+      </label>
+      <input type="file" id="attachments" multiple @change="handleFileUpload" class="file-input" />
+      <div v-if="attachments.length > 0">
+        <h3>
+          <i class="fas fa-paperclip"></i> Attachments:
+        </h3>
+        <ul>
+          <li v-for="(attachment, index) in attachments" :key="index">
+            <i class="fas fa-file"></i>{{ attachment }}
+          </li>
+        </ul>
+      </div>
+    </div>
 
-      
-        <div class="priorities">
-          <h3 class="priority-title" for="priority" >Priority</h3>
-          <input class="priority-input" name="exampleRadios" type="radio" id="p1" value="1"/>
-            <label class="priority-input" id="priority" for="p1">1 (High)</label>
-          <input class="priority-input" name="exampleRadios" type="radio" id="p2" value="2"/>
-            <label class="priority-input"  id="priority" for="p2">2</label>
-          <input class="priority-input" name="exampleRadios" type="radio" id="p3" value="3"/>
-            <label class="priority-input"  id="priority"  for="p3">3 (Medium)</label>
-          <input class="priority-input" name="exampleRadios" type="radio" id="p4" value="4"/>
-            <label class="priority-input"  id="priority"  for="p4">4</label>
-          <input class="priority-input" name="exampleRadios" type="radio" id="p5" value="5"/>
-            <label class="priority-input"  id="priority"  for="p5">5 (Low)</label>
-        </div>
-        <div class="choosefile">
-          <label class="attachment" for="file">Attachment <i class="fa fa-paperclip"></i></label>
-          <input type="file" id="file" multiple />
-        </div>
-        <div class="send-draft">
-          <button class="send-button" @click="sendEmail">Send <i class="fa fa-paper-plane"></i> </button>
-          <button class="draft-button" @click="saveDraft">Save as draft <i class="fa fa-file"></i></button>
-        </div>
-    </transition-group>
-    {{ profileContactInfo }}
+    <button @click="sendEmail" class="send-button">
+      <i class="fas fa-paper-plane"></i> Send Email
+    </button>
+    <button @click="saveDraft" class="send-button">
+        <i class="fas fa-file"></i> Save Draft
+      </button>
+    <div>
+      <!-- <button @click="generateDateTime">Generate Date & Time</button> -->
+      <p>{{ dateTime }}</p>
+    </div>
+  </div>
 </template>
 <script>
-import { ref } from 'vue';
-// const to = document.getElementById('to').value;
-// const subject = document.getElementById('subject').value;
-// const message = document.getElementById('message').value;
-// const priority = document.getElementById('priority').value;
+import { ref, watchEffect } from 'vue';
+import { useRouter } from 'vue-router';
+import { format } from 'date-fns';
+const router = useRouter();
 export default {
-  props: {
-    profileContactInfo: String,
-  },
-setup() {
-  const emailData = ref({
-    to: '',
-    subject: '',
-    message: '',
-    priority: 1, // default priority
-    file: null,
-  });
-
-  const sendEmail = async () => {
-    const apiKey = 'YOUR_SENDGRID_API_KEY'; // Replace with your SendGrid API key
-    const apiUrl = 'https://api.sendgrid.com/v3/mail/send';
-
-    const data = {
-      personalizations: [{
-        to: [{ email: emailData.value.to }],
-        subject: emailData.value.subject,
-      }],
-      from: { email: 'your-email@example.com' }, // Replace with your email
-      content: [{
-        type: 'text/plain',
-        value: emailData.value.message,
-      }],
+  props: ['profileContactInfo'],
+  data() {
+    return {
+      from: '',
+      emailBody: '',
+      attachments: [],
+      showAddUserModal: false,
+      newUserEmail: '',
+      to: [],
+      dateTime: '',
     };
-  };
-}
-}
+  },
+  mounted() {
+    this.from = this.profileContactInfo;
+  },
+  methods: {
+    handleFileUpload(event) {
+      const files = event.target.files;
+      this.attachments = Array.from(files).map(file => file.name);
+    },
+    async sendEmail() {
+      console.log('To:', this.to);
+      console.log('From:', this.from);
+      console.log('Sending email with body:', this.emailBody);
+      console.log('Attachments:', this.attachments);
+      const currentDate = new Date();
+      const formattedDateTime = format(currentDate, "yyyy-MM-dd h:mm a");
+      this.dateTime = formattedDateTime;
+      try {
+        const response = await fetch('http://localhost:8081/mail/sendEmail', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            to: this.to,
+            from: this.from,
+            emailBody: this.emailBody,
+            attachments: this.attachments,
+            dateTime: this.dateTime,
+          }),
+        });
 
+        if (response.ok) {
+          console.log('Email sent successfully');
+        } else {
+          console.error('Failed to send email');
+        }
+      } catch (error) {
+        console.error('Error sending email:', error);
+      }
+    },
+    openAddUserModal() {
+      this.showAddUserModal = true;
+    },
+    closeAddUserModal() {
+      this.showAddUserModal = false;
+    },
+    addUser() {
+      console.log('Adding user with email:', this.newUserEmail);
+      this.to.push({ email: this.newUserEmail });
+      this.closeAddUserModal();
+    },
+    saveDraft() {
+      console.log('To:', this.to);
+      console.log('From:', this.from);
+      console.log('Saving draft with body:', this.emailBody);
+      console.log('Attachments:', this.attachments);
+      const currentDate = new Date();
+      const formattedDateTime = format(currentDate, "yyyy-MM-dd h:mm a");
+      this.dateTime = formattedDateTime;
+      const draft = {
+        to: this.to,
+        from: this.from,
+        emailBody: this.emailBody,
+        attachments: this.attachments,
+        dateTime: this.dateTime,
+      };
+      // Convert the draft object to a JSON string
+      const draftJson = JSON.stringify(draft);
+      localStorage.setItem('emailDraft', draftJson);
+      console.log('Draft saved successfully');
+      console.log('Draft:', draft);
+    },
+    
+  },
+};
 </script>
 
+
 <style scoped>
-.compose-title {
-  font-size: 24px;
-  margin-bottom: 20px;
-  color: #333;
+.compose-container {
+  max-width: 900px;
+  max-height: 900px;
+  margin: 50px auto;
+  padding: 20px;
+  background-color: #ffffff;
+  border-radius: 12px;
+  box-shadow: 0 6px 6px rgba(4, 4, 4, 0.1);
 }
 
-.priority-title {
-  font-weight: bold;
-  color: #007BFF;
-  font-size: 20px;
-  margin: 0px;
-}
-
-.sender, .subject, .messagebox, .choosefile, .priorities, .send-draft {
-  border: 1px solid #ddd;
-  padding: 15px;
+/* Heading styling */
+h2 {
+  color: #187dc1;
+  font-size: 28px;
   margin-bottom: 15px;
-  background-color: #fff;
-  border-radius: 8px;
-  transition: transform 0.3s ease-in-out;
 }
 
-.sender:hover, .subject:hover, .messagebox:hover, .choosefile:hover, .priorities:hover, .send-draft:hover {
-  transform: scale(1.02);
+/* Textarea styling */
+.compose-textarea {
+  width: 600px;
+  height: 300px;
+  padding: 10px;
+  margin-bottom: 15px;
+  margin-left: 15px;
+  background-color: #ffffff;
+  box-shadow: 0 2px 4px rgba(255, 255, 255, 0.1), 0 4px 8px rgba(0, 0, 0, 0);
+  border-radius: 6px;
+  font-size: 16px;
+  color: #000000;
+  transition: box-shadow 0.3s ease-in-out;
 }
 
-label {
-  font-weight: bold;
-  color: #007BFF;
-  font-size: larger;
-  margin: 0px;
+/* Attachment styling */
+.attachment-container {
+  margin-bottom: 20px;
 }
 
-#to, #subject, .messagebox {
+.file-input {
   width: 100%;
-  padding: 12px 20px;
-  margin: 4px 0;
-  box-sizing: border-box;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  font-size: 17px;
+  padding: 10px;
+  margin-bottom: 10px;
+  border-radius: 6px;
 }
 
-.messagebox {
-  height: 200px;
-  font-size: 30px;
+.file-input:hover {
+  border-color: #ffffff;
+  color: black;
 }
 
-.priority-input {
-  margin: 8px;
+.modal {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
 }
 
-.attachment {
-  font-size: larger;
-  margin: 7px;
-  
+.modal-content {
+  background-color: #ffffff;
+  padding: 20px;
+  border-radius: 6px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  max-width: 600px;
+  width: 90%;
+  font-family: Arial, sans-serif;
+  position: relative;
+  /* Added to handle positioning of the button */
 }
 
-#file {
-  margin: 15px;
-  font-size: medium;
-  color:#333;
-  font-weight: bolder;
-}
-.send-button, .draft-button {
-  background-color: #007BFF;
+/* Add User button styling */
+.add-user-button {
+  background-color: #3498db;
+  color: #ffffff;
+  padding: 10px 20px;
   border: none;
-  color: white;
-  padding: 15px 30px;
-  margin: 8px;
-  text-decoration: none;
-  border-radius: 5px;
+  border-radius: 6px;
   cursor: pointer;
-  font-size: large;
+  font-size: 16px;
+  margin-top: 10px;
+  /* Adjust as needed */
+  transition: background-color 0.3s ease-in-out;
 }
 
-.send-button:hover, .draft-button:hover {
-  background-color: #ddd;
-  color: #007BFF;
+.add-user-button:hover {
+  background-color: #2980b9;
 }
-</style>
+
+.close {
+  color: #ff0000;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  font-size: 28px;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.close:hover,
+.close:focus {
+  color: #ca0202;
+}
+
+/* Button styling */
+.send-button {
+  background-color: #3498db;
+  color: #ffffff;
+  padding: 12px 24px;
+  border: none;
+  border-radius: 6px;
+  cursor: pointer;
+  font-size: 16px;
+  transition: background-color 0.3s ease-in-out;
+  margin: 0 15px;
+}
+
+.send-button:hover {
+  background-color: #2ecc71;
+}
+
+.input-group,
+.to-group,
+.from-group {
+  margin-bottom: 15px;
+  display: flex;
+  flex-direction: column;
+}
+
+.input-group label,
+.to-group label,
+.from-group label {
+  margin-bottom: 10px;
+  font-size: 16px;
+  color: #000000;
+}
+
+.input-group input,
+.to-group input,
+.from-group input {
+  width: 80%;
+  padding: 10px;
+  font-size: 16px;
+  border: 2px solid #ffffff;
+  border-radius: 6px;
+  transition: border-color 0.3s ease-in-out;
+}
+
+.input-group input:focus,
+.to-group input:focus,
+.from-group input:focus {
+  border-color: #ffffff;
+}
+
+.add-recipient {
+  cursor: pointer;
+  display: inline-block;
+  margin-left: 10px;
+  font-size: 20px;
+  color: #3498db;
+}
+
+.add-recipient:hover {
+  color: #2ecc71;
+}
+
+/* User list styling */
+.to-info {
+  margin-top: 10px;
+}
+
+.to-info ul {
+  list-style-type: none;
+  color: black;
+  padding: 0;
+}
+
+.to-info ul li {
+  font-size: 16px;
+  color: #000000;
+  margin-bottom: 5px;
+  display: flex;
+  align-items: center;
+}
+
+.to-info ul li i {
+  margin-right: 5px;
+}</style>
