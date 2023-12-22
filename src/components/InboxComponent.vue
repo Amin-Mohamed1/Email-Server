190132e4df82e6d3047847e33b6644fdaad525be
Amen Mohamed <145727573+WorldisAmen@users.noreply.github.com>
2023-12-22 16:01:10 +0200

    Semi done inbox

--- src/components/InboxComponent.vue
+++ src/components/InboxComponent.vue
@@ -1,6 +1,44 @@
 <template>
   <div>
     <h2 class="inbox-title">Inbox</h2>
+    <div class="search-bars-container">
+      <div class="search-bar">
+        <select v-model="searchCategory" class="search-category">
+          <option value="sender">Sender</option>
+          <option value="body">Body</option>
+          <option value="date">Date</option>
+          <option value="date">Attachments</option>
+          <option value="subject">Subject</option>
+        </select>
+        <input v-model="searchQuery" type="text" placeholder="Search Query...">
+        <button @click="search" class="search-btn">
+          <i class="fas fa-search"></i>
+        </button>
+      </div>
+
+      <div class="sort-bar">
+        <select v-model="sortCategory" class="sort-category">
+          <option value="sender">Sender</option>
+          <option value="priority">Priority</option>
+          <option value="subject">Subject</option>
+        </select>
+        <i class="fas fa-sort-alpha-down sort-icon" :class="{ 'rotate-icon': sortOrder === 'asc' }"></i>
+      </div>
+
+      <div class="filter-bar">
+        <select v-model="filterCategory" class="filter-category">
+          <option value="sender">Sender</option>
+          <option value="date">Date</option>
+          <option value="subject">Subject</option>
+        </select>
+        <input v-model="filterQuery" type="text" placeholder="Filter Query...">
+        <button @click="filter" class="filter-btn">
+          <i class="fas fa-filter"></i>
+        </button>
+      </div>
+
+    </div>
+
     <transition-group name="fade" mode="out-in">
       <div v-for="email in displayedEmails" :key="email.id" class="email-item" @click="expandBody(email)">
         <div class="info">
@@ -41,19 +79,24 @@
       <button @click="changePage('next')" :disabled="currentPage === totalPages">Next</button>
     </div>
   </div>
-  {{ props.profileContactInfo }}
-  {{ props.Inboxemails }}
 </template>
 
 <script setup>
+
 import { ref, onMounted, computed } from 'vue';
 import axios from 'axios';
-
-const currentUserEmail = 'user@gmail.com';
-const emailsPerPage = 5;
 import { defineProps } from 'vue';
 
-const props = defineProps(['profileContactInfo','Inboxemails']);
+let searchCategory = 'sender';
+let defaultSortCategory = 'priority';
+let sortCategory = ref(defaultSortCategory);
+let defaultSortOrder = 'asc';
+let sortOrder = ref(defaultSortOrder);
+let filterCategory = 'sender';
+const currentUserEmail = 'user@gmail.com';
+const emailsPerPage = 5;
+
+const props = defineProps(['profileContactInfo', 'Inboxemails']);
 
 const fetchEmails = async () => {
   try {
@@ -64,16 +107,19 @@ const fetchEmails = async () => {
   }
 };
 
-const emails = ref([
-  { id: 1, sender: 'Albert.Einstein@gmail.com', subject: 'Meeting Tomorrow', body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ...', attachments: [{ name: 'project_plan.png', type: 'png' }, { name: 'meeting_notes.docx', type: 'docx' }], priority: 'Medium', dateTime: '2023-01-02 2:30 PM'},
-  { id: 2, sender: 'Robert.Oppenheimer@gmail.com', subject: 'Presentation Feedback', body: 'Your presentation was excellent!', dateTime: '2023-01-02 2:30 PM', priority: 'Medium', attachments: [{ name: 'feedback.pdf', type: 'pdf' }, { name: 'slides.pdf', type: 'pdf' }] },
-  { id: 3, sender: 'Neils.Bohr@gmail.com', subject: 'Meeting Tomorrow', body: 'Hi, let\'s discuss the project tomorrow.', dateTime: '2023-01-01 10:00 AM', priority: 'High', attachments: [] },
-  { id: 4, sender: 'Lionel.Messi@gmail.com', subject: 'Presentation Feedback', body: 'Your presentation was excellent!', dateTime: '2023-01-02 2:30 PM', priority: 'Medium', attachments: [] },
-  { id: 1, sender: 'Albert.Einstein@gmail.com', subject: 'Meeting Tomorrow', body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ...', attachments: [{ name: 'project_plan.png', type: 'png' }, { name: 'meeting_notes.docx', type: 'docx' }], priority: 'Medium', dateTime: '2023-01-02 2:30 PM'},
-  { id: 2, sender: 'Robert.Oppenheimer@gmail.com', subject: 'Presentation Feedback', body: 'Your presentation was excellent!', dateTime: '2023-01-02 2:30 PM', priority: 'Medium', attachments: [{ name: 'feedback.pdf', type: 'pdf' }, { name: 'slides.pdf', type: 'pdf' }] },
-  { id: 3, sender: 'Neils.Bohr@gmail.com', subject: 'Meeting Tomorrow', body: 'Hi, let\'s discuss the project tomorrow.', dateTime: '2023-01-01 10:00 AM', priority: 'High', attachments: [] },
-  { id: 4, sender: 'Lionel.Messi@gmail.com', subject: 'Presentation Feedback', body: 'Your presentation was excellent!', dateTime: '2023-01-02 2:30 PM', priority: 'Medium', attachments: [] },
-]);
+const emails = ref([]);
+props.Inboxemails.forEach((inboxEmail) => {
+  const transformedEmail = {
+    id: inboxEmail.id,
+    sender: inboxEmail.sender,
+    subject: inboxEmail.subject,
+    body: inboxEmail.body,
+    attachments: inboxEmail.attatchments || [],
+    priority: inboxEmail.priority,
+    dateTime: inboxEmail.dateTime,
+  };
+  emails.value.push(transformedEmail);
+});
 
 onMounted(() => {
   fetchEmails();
@@ -114,7 +160,21 @@ const changePage = (direction) => {
   }
 };
 </script>
+
 <style scoped>
+
+
+.sort-icon {
+  margin-top: 14px;
+}
+
+
+.search-bars-container {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 20px; /* Adjust this margin as needed */
+  }
+
   .inbox-title {
     font-size: 24px;
     margin-bottom: 20px;
@@ -191,6 +251,8 @@ const changePage = (direction) => {
     max-height: 3em; 
     line-height: 1.5em;
   }
+
+  
   
   .truncated-body span {
     cursor: pointer;
@@ -235,7 +297,220 @@ ul {
 
   .pagination button:disabled {
     cursor: not-allowed;
-    background-color: #eee;
+    background-color: #ffffff;
   }
-  </style>
+  .sort-bar {
+    display: flex;
+    margin-top: 20px;
+    transition: transform 0.3s;
+    margin-bottom: 20px;
+    margin-left: 2px;
+    margin-right: 2px;
+    width: 600px;
+    border: 1px solid #ffffff;
+    border-radius: 4px;
+    overflow: hidden;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  }
+
+  .sort-bar:hover {
+    transform: scale(1.05);
+  }
+
+  .sort-category {
+    padding: 10px;
+    border: 1px solid #a3a3a3;
+    border-radius: 4px;
+    background-color: #a3a3a3;
+    color: #000000;
+    cursor: pointer;
+    appearance: none;
+    width:170px;
+    -webkit-appearance: none;
+    background-image: url('data:image/svg+xml;utf8,<svg fill="%23333" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M7 10l5 5 5-5z"/><path d="M0 0h24v24H0z" fill="none"/></svg>'); /* Add custom arrow icon */
+    background-repeat: no-repeat;
+    background-position: right 10px center;
+  }
+
+  .sort-category:focus {
+    outline: none;
+    border-color: #a3a3a3;
+  }
+
+  .sort-bar,
+  .sort-bar input {
+    transition: border-color 0.3s;
+  }
+
+  .sort-bar  {
+    padding: 8px;
+    border: 0px solid #ffffff;
+    border-radius: 4px;
+    margin-left: 2px;
+    margin-right: 2px;
+    color: #000000;
+    background-color: #a3a3a3;
+    outline: none;
+    width:16%;
+  }
+
+  .sort-bar input {
+    flex: 1;
+    padding: 8px;
+    border: 1px solid #ffffff;
+    border-radius: 4px;
+    margin-right: 10px;
+    color: #000000;
+    outline: none;
+  }
+
+  .sort-bar .sort-btn {
+    background-color: #007BFF;
+    color: #fff;
+    border: none;
+    padding: 8px;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+    outline: none;
+  }
+
+  .sort-bar .sort-btn i {
+    font-size: 18px;
+  }
+
+  .sort-bar .sort-input::placeholder {
+    color: #007BFF;
+  }
+
+  .sort-bar,
+  .sort-bar input:focus {
+    border-color: #053a72;
+  }
+
+  .search-bar{
+    display: flex;
+    margin-top: 20px;
+    transition: transform 0.3s;
+    margin-bottom: 20px;
+    margin-left: 2px;
+    margin-right: 2px;
+    width: 600px;
+    border: 1px solid #ffffff;
+    border-radius: 4px;
+    overflow: hidden;
+    width:45%;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  }
+    .filter-bar {
+    display: flex;
+    margin-top: 20px;
+    transition: transform 0.3s;
+    margin-bottom: 20px;
+    margin-left: 10px;
+    margin-right: 2px;
+    width: 600px;
+    border: 1px solid #ffffff;
+    border-radius: 4px;
+    overflow: hidden;
+    width:30px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  }
+
+
+  .search-bar:hover, .filter-bar:hover {
+    transform: scale(1.05);
+  }
+
+  .search-category{
+    padding: 10px;
+    border: 1px solid #a3a3a3;
+    border-radius: 4px;
+    background-color: #a3a3a3;
+    color: #000000;
+    cursor: pointer;
+    appearance: none;
+    width:150px;
+    -webkit-appearance: none;
+    background-image: url('data:image/svg+xml;utf8,<svg fill="%23333" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M7 10l5 5 5-5z"/><path d="M0 0h24v24H0z" fill="none"/></svg>'); /* Add custom arrow icon */
+    background-repeat: no-repeat;
+    background-position: right 10px center;
+  }
+
+   .filter-category {
+    padding: 10px;
+    border: 1px solid #a3a3a3;
+    border-radius: 4px;
+    background-color: #a3a3a3;
+    color: #000000;
+    cursor: pointer;
+    appearance: none;
+    width:100px;
+    -webkit-appearance: none;
+    background-image: url('data:image/svg+xml;utf8,<svg fill="%23333" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M7 10l5 5 5-5z"/><path d="M0 0h24v24H0z" fill="none"/></svg>'); /* Add custom arrow icon */
+    background-repeat: no-repeat;
+    background-position: right 10px center;
+  }
+
+
+  .search-category:focus, .filter-category:focus {
+    outline: none;
+    border-color: #a3a3a3;
+  }
+
+  .search-bar,
+  .search-bar input, 
+  .filter-bar,
+  .filter-bar input
+  {
+    transition: border-color 0.3s;
+  }
+
+  .search-bar , .filter-bar  {
+    padding: 8px;
+    border: 0px solid #ffffff;
+    border-radius: 4px;
+    margin-right: 10px;
+    width:600px;
+    color: #000000;
+    background-color: #a3a3a3;
+    outline: none;
+  }
+
+  .search-bar input, .filter-bar input  {
+    flex: 1;
+    padding: 8px;
+    border: 1px solid #ffffff;
+    border-radius: 4px;
+    margin-right: 10px;
+    color: #000000;
+    outline: none;
+  }
+
+  .search-bar .search-btn, .filter-bar .filter-btn {
+    background-color: #007BFF;
+    color: #fff;
+    border: none;
+    padding: 8px;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+    outline: none;
+  }
+
+  .search-bar .search-btn i, .filter-bar .filter-btn i  {
+    font-size: 18px;
+  }
+
+  .search-bar .search-input::placeholder , .filter-bar .filter-input::placeholder {
+    color: #007BFF;
+  }
+
+  .search-bar,
+  .search-bar input:focus, .filter-bar,
+  .filter-bar input:focus{
+    border-color: #053a72;
+  }
+
+</style>
   
